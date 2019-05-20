2a6605c809bf77ab3c2419cd64a67b7997ffc4e8
Hugo Denizart <thepoon@cartooncraft.fr>
2019-05-20 09:22:01 +0200

    Fix API entry-point (#1)
    TY

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,19 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-import Nuxt from 'nuxt';
-import * as Config from '../nuxt.config';
 
 const App = new Koa();
 const AppRouter = new Router();
-const nuxt = new Nuxt(Config);
 
 AppRouter.get("/test", (ctx) => {
     ctx.body = {
         status: 'success',
         message: 'hello'
     }
-    console.log('Good job.')
 });
 
-App.use(async ctx => {
-    ctx.status = 200
-    ctx.respond = false // Mark request as handled for Koa
-    nuxt.render(ctx.req, ctx.res)
-});
 App.use(AppRouter.routes());
 
 export default {
     path: "/api",
-    handler: App,
+    handler: App.callback(),
 }
