d33d6811dad61fef6aea705706356adf81e081b2
milaneyia <39941577+milaneyia@users.noreply.github.com>
2020-04-02 01:40:41 -0300

    right side

--- pages/index.vue
+++ pages/index.vue
@@ -49,29 +49,68 @@
         </div>
 
         <div class="right-side">
-            <!---TODO: SETTLE ON A DESIGN AND REPLACE PLACEHOLDER--->
-            <div class="headings">
-                test
-            </div>
-                
-            <div class="date">
-                01/01/20 10PST - 01/13/20 10 PST
+            <div class="right-side__title">
+                standard | voting
             </div>
 
-            <!---TODO: STYLE TEXTBOX IF NEEDED--->
-            <div class="desc">
-                Mapper's Choice Awards is back for round 4 in 2019! This is a voting event where all mappers/modders can nominate and <br> 
-                vote what they think is the best map/mapper for each category. <br><br>
+            <div class="wrap">
+                <div class="right-side__general">
+                    <div class="right-side__general__ranked-sets box">
+                        <div>
+                            <small>RANKED SETS</small>
+                        </div>
+                        <div style="border: 1px solid white; width: 1px; height: 100%" />
+                        <div style="font-family: Scoreboard; font-size: 2.8rem">
+                            000
+                            | 000
+                        </div>
+                    </div>
 
-                Our intention is to give a new perspective on the best maps of 2019 through the eyes of the mapping community! <br><br>
+                    <div class="right-side__general__vote box">
+                        vote now ! <span style="margin-left: 15px">>></span>
+                    </div>
+                </div>
 
-                This year, we have separated storyboarding from the other modes to give them more emphasis, as well as going with a Google Form submission, <br>
-                making it as easy as possible for you to vote/nominate! <br><br>
-
-                We hope as many participants as possible take part in this event!
+                <div class="right-side__categories-container">
+                    <div
+                        v-for="i in 3"
+                        :key="i"
+                        class="right-side__categories"
+                    >
+                        <div class="right-side__categories__title">
+                            map categories
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                        <div class="right-side__categories__award">
+                            grand award
+                        </div>
+                    </div>
+                </div>
+                    
+                <div class="right-side__organizers-container box">
+                    <div class="right-side__organizers-title">
+                        <small>ORGANIZED BY</small>
+                    </div>
+                    <div class="right-side__organizers">
+                        person a, person b, person c, person d, person e
+                    </div>
+                </div>
             </div>
         </div>
-        <!---TODO: GET IMAGE ASSETS FOR MODE ICONS--->
     </div>
 </template>
 
@@ -153,12 +192,6 @@ export default Vue.extend({
 	margin-bottom: 5%;
 }
 
-.headings {
-	margin-top: 5%;
-	font-family: 'Lexend Peta', bold;
-	font-size: 4vw;
-}
-
 .left-side {
     padding-right: 35px;
     padding-top: 7%;
@@ -182,8 +215,14 @@ export default Vue.extend({
 .left-side__text-box {
     border-radius: 0 15px 15px 0; 
     background-color: rgba(0, 0, 0, 0.66); 
-    text-align: left; 
     padding: 45px 30px;
+    display: flex;
+}
+
+.box {
+    border-radius: 15px; 
+    background-color: rgba(0, 0, 0, 0.66); 
+    padding: 12px;
     display: flex;
 }
 
@@ -191,6 +230,81 @@ export default Vue.extend({
     padding-left: 35px;
     flex: 0 0 100%;
     width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.right-side__title {
+    font-family: 'Lexend Peta', bold;
+    font-size: 2rem;
+    text-shadow: 0 0 4px white;
+    margin: 5% 25px 10px auto;
+}
+
+.right-side__general, 
+.right-side__categories-container, 
+.right-side__organizers-container {
+    display: flex;
+}
+
+.wrap {
+    border-top: 3px solid var(--red-pink);
+    border-left: 3px solid var(--red-pink);
+    border-bottom: 3px solid var(--red-pink);
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+    padding: 25px;
+}
+
+.right-side__general,
+.right-side__categories-container {
+    margin-bottom: 40px;
+    justify-content: space-around;
+}
+
+.right-side__general__ranked-sets {
+    flex: 1 1 50%;
+    margin-right: 10px;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.right-side__general__vote {
+    flex: 1 1 50%;
+    margin-left: 10px;
+    color: var(--red-pink);
+    font-style: italic;
+    font-size: 1.7rem;
+    justify-content: flex-end;
+    align-items: center;
+    text-shadow: 1px 1px 3px #222;
+    font-weight: 900;
+    letter-spacing: 1.2px;
+    background-color: white;
+    /* background: linear-gradient(135deg,black 0%, black 20%, white 20%, white 22%, black 22%, black 24%, white 24%, white 26%, var(--red-pink) 26%, var(--red-pink) 28%, white 28%); */
+}
+
+.right-side__categories__title {
+    border-bottom: 2px solid #fff;
+    margin-bottom: 15px;
+    font-weight: bold;
+    font-size: 1.5rem;
+}
+
+.right-side__categories__award {
+    margin-bottom: 8px;
+}
+
+.right-side__organizers-container {
+    flex-direction: column;
+}
+
+.right-side__organizers-title {
+    border-bottom: 1px solid white;
+}
+
+.right-side__organizers {
+    padding: 25px;
 }
 
 .wheel {
@@ -211,7 +325,7 @@ export default Vue.extend({
 }
 
 .wheel__box {
-    box-shadow: inset 0 0 20px 0px black;
+    box-shadow: inset 0 0 20px 0px #222;
     border: 3px solid rgba(0, 0, 0, 0.3);
     border-radius: 15px;
     border-bottom: 2px solid white;
